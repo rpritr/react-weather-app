63f078444bb5d44a3484cc1aec4badf42ebfb16b
rpritr <robi@foreach-labs.si>
2026-01-09 08:30:38 +0100

    add weather temp.

--- src/Components/Weather/Weather.js
+++ src/Components/Weather/Weather.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Weather.css';
 import WeatherHeader from "./WeatherHeader/WeatherHeader";
+import WeatherTemperature from "./WeatherTemperature/WeatherTemperature";
 const Weather = (props) => {
     const sunny = new URL('../../assets/sunny.svg', import.meta.url);
 
     return (
         <div className="weather">
             <WeatherHeader icon={sunny} location="Valle de Angeles, HN" date="Monday 01/17/2022"/>
+            <WeatherTemperature temperature={15} description="Mostly sunny"/>
+
         </div>
     )
 }
